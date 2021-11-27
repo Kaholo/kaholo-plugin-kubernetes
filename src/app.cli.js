731124c1c61fcd92fa834660448c706c3b97b4bf
Amit Alon <amit@kaholo.io>
2021-11-27 21:52:30 +0200

    Merged Kubernetes and Kubernetes CLI into one plugin.

---
+++ src/app.cli.js
@@ -0,0 +1,71 @@
+
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+const os = require('os')
+
+function _getsudo(params,settings){
+  return ((os.platform() == 'linux') && (params.SUDO || settings.SUDO )) ? 'sudo' :''
+}
+
+async function exposeDeployment(action,settings) {
+  const params = action.params;
+  const type = params.TYPE || 'NodePort'
+  const execString = `${_getsudo(params,settings)} kubectl expose deployment ${params.DEPLOYMENT} --type=${type} --name=${params.NAME}`
+  return exec(execString);
+}
+
+async function getServices(action,settings) {
+  const params = action.params;
+  const execString = `${_getsudo(params,settings)} kubectl get svc -n ${action.params.namespace} -o json`
+  const execResult = await exec(execString);
+  try{
+    return JSON.parse(execResult.stdout);
+  } catch (err){
+    throw `Could not parse "get svc" output`;
+  }
+}
+
+async function getPods(action,settings){
+  const SUDO = _getsudo(action.params,settings)
+  const execString = ` ${SUDO} kubectl get pod`
+  const res = await exec(execString);
+  if(!res.stdout.includes('READY')){
+    return Promise.resolve(res);
+  }
+  console.log(res.stdout)
+  const spaces = /\s\s+/g;
+  const resultsArr = res.stdout.split('\n',res.stdout.replace(/[^'\n']/g, "").length)
+  const titles = resultsArr[0].split(spaces)
+  const newResult = []
+  const newRow = {}
+  for (let i = 1; i < resultsArr.length; i++) {
+    const row = resultsArr[i].split(spaces);
+    titles.forEach((title,index)=>{
+      newRow[title] = row[index]
+    })
+    newResult.push(newRow)
+  }
+  newResult.stdout = newResult 
+  console.log("Output:");
+  
+  return Promise.resolve(newResult)
+}
+
+async function apply(action,settings){
+  const SUDO = _getsudo(action.params,settings);
+  const force = action.params.FORCE ? '--force' : '';
+  const noOverwrite = action.params.NO_OVERWRITE ? '--overwrite=false' : '';
+  const dryRun = action.params.DRY_RUN ? `--dry-run=${action.params.DRY_RUN.trim()}` : '';
+  const args = [force, noOverwrite, dryRun].filter(arg => arg);
+  const execString = `${SUDO} kubectl apply -f ${action.params.FILE_PATH} ${args.join(' ')}`;
+  return exec(execString);
+}
+
+
+module.exports = {
+  cliExposeDeployment: exposeDeployment,
+  cliGetPods: getPods,
+  cliApply: apply,
+  cliGetServices: getServices
+};
+
