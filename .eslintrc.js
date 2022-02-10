287ee4ae64816a4c1d0be764ac2e30a0f39f156f
Matan Kadosh <kadosh10001@gmail.com>
2022-02-10 16:58:53 +0200

    lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,21 @@
 module.exports = {
-    env: {
-      browser: true,
-      commonjs: true,
-      es2021: true,
-      jest: true,
-    },
-    extends: [
-      "airbnb-base",
-    ],
-    parserOptions: {
-      ecmaVersion: "latest",
-    },
-    rules: {
-      quotes: ["error", "double"],
-      curly: ["error", "all"],
-    },
-  };
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    "airbnb-base",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    quotes: ["error", "double"],
+    curly: ["error", "all"],
+    "no-throw-literal": ["off"],
+    "no-console": "off",
+    "no-restricted-syntax": ["error", "FunctionExpression", "WithStatement"],
+  },
+};
--- app.js
+++ app.js
@@ -1,13 +1,25 @@
+/* eslint-disable no-await-in-loop */
+const yaml = require("js-yaml");
+const fs = require("fs");
+const { KubernetesObjectApi, CoreV1Api } = require("@kubernetes/client-node");
+const {
+  getConfig, parseArr, getDeleteFunc, runDeleteFunc, parseErr,
+} = require("./helpers");
+const cliApp = require("./app.cli");
 
-const yaml = require('js-yaml');
-const fs = require('fs');
-const {getConfig, parseArr, getDeleteFunc, runDeleteFunc, parseErr} = require("./helpers");
-const {KubernetesObjectApi, CoreV1Api} = require('@kubernetes/client-node');
+async function applyBySpec(client, spec) {
+  try {
+    await client.read(spec);
+  } catch (err) {
+    return client.create(spec);
+  }
+  return client.patch(spec);
+}
 
-async function apply(action, settings){
+async function apply(action, settings) {
   const yamlPath = (action.params.yamlPath || "").trim();
   const namespace = (action.params.namespace || "").trim();
-  if (!yamlPath){
+  if (!yamlPath) {
     throw "Not given yaml file path";
   }
   /**
@@ -18,18 +30,17 @@ async function apply(action, settings){
   const kc = getConfig(action.params, settings);
   const client = kc.makeApiClient(KubernetesObjectApi);
   const created = [];
-  for (const spec of specs){
-    if (namespace && !spec.metadata.namespace && spec.kind !== "Namespace"){
+  for (const spec of specs) {
+    if (namespace && !spec.metadata.namespace && spec.kind !== "Namespace") {
       spec.metadata.namespace = namespace;
     }
     spec.metadata.annotations = spec.metadata.annotations || {};
-    delete spec.metadata.annotations['kubectl.kubernetes.io/last-applied-configuration'];
-    spec.metadata.annotations['kubectl.kubernetes.io/last-applied-configuration'] = JSON.stringify(spec);
+    delete spec.metadata.annotations["kubectl.kubernetes.io/last-applied-configuration"];
+    spec.metadata.annotations["kubectl.kubernetes.io/last-applied-configuration"] = JSON.stringify(spec);
     try {
       const response = await applyBySpec(client, spec);
       created.push(response.body);
-    } 
-    catch (err) {
+    } catch (err) {
       created.push(parseErr(err));
       throw created;
     }
@@ -37,83 +48,72 @@ async function apply(action, settings){
   return created;
 }
 
-async function applyBySpec(client, spec){
-  try {
-    await client.read(spec);
-  }
-  catch (err){
-    return client.create(spec);
-  }
-  return client.patch(spec);
-}
-
-async function deleteObject(action, settings){  
+async function deleteObject(action, settings) {
   const types = parseArr(action.params.types);
   const names = parseArr(action.params.names);
   const namespace = (action.params.namespace || "").trim();
-  if (types.length < 1 || names.length < 1){
+  if (types.length < 1 || names.length < 1) {
     throw "One of the required parameters was not passed!";
   }
   const kc = getConfig(action.params, settings);
 
-  const [promises, deleted, failed]  = [[],[],[]]; // initiate with empty lists
-  const deleteFuncs = types.map(resourceType => {
+  const [promises, deleted, failed] = [[], [], []]; // initiate with empty lists
+  const deleteFuncs = types.map((resourceType) => {
     const deleteFunc = getDeleteFunc(kc, resourceType);
-    const namespaced = deleteFunc.name.includes("Namespaced")
-    if (namespaced && !namespace){
+    const namespaced = deleteFunc.name.includes("Namespaced");
+    if (namespaced && !namespace) {
       throw `Must specify namespace to delete object of type '${resourceType}`;
     }
-    return {deleteFunc, resourceType, namespaced};
+    return { deleteFunc, resourceType, namespaced };
   });
-  deleteFuncs.forEach(({deleteFunc, resourceType, namespaced}) => {
-    names.forEach(name => {
+  deleteFuncs.forEach(({ deleteFunc, resourceType, namespaced }) => {
+    names.forEach((name) => {
       // to run all deletes at once
       promises.push(runDeleteFunc(deleteFunc, resourceType, name, namespaced ? namespace : null));
     });
   });
   const results = (await Promise.all(promises)); // remove all empty results
-  results.forEach(deleteObj => {
-    if (deleteObj.err){
+  results.forEach((deleteObj) => {
+    if (deleteObj.err) {
       failed.push(deleteObj);
-    }
-    else {
+    } else {
       deleted.push(deleteObj);
     }
   });
-  const returnVal = {deleted, failed};
-  if (failed.length > 0 || deleted.length === 0){
+  const returnVal = { deleted, failed };
+  if (failed.length > 0 || deleted.length === 0) {
     throw returnVal;
   }
   return returnVal;
 }
 
-async function getAllServices(action, settings){
+async function getAllServices(action, settings) {
   const { namespace } = action.params;
   const kc = getConfig(action.params, settings);
   const client = kc.makeApiClient(CoreV1Api);
   try {
-    if (namespace=='*'){
+    if (namespace === "*") {
       const namespaces = await client.listNamespace();
-      const [...serviceResults] = await Promise.all(namespaces.body.items.map(namespaceObj=>{
-        return client.listNamespacedService(namespaceObj.metadata.name)
-      }));
-      const allServices = []
-      serviceResults.forEach(serviceResult=>{
+      const listServicesPromises = namespaces.body.items.map(
+        (namespaceObj) => client.listNamespacedService(namespaceObj.metadata.name),
+      );
+      const [...serviceResults] = await Promise.all(listServicesPromises);
+      const allServices = [];
+      serviceResults.forEach((serviceResult) => {
         allServices.push(...serviceResult.body.items);
-      })
+      });
       return allServices;
     }
-    const res = await client.listNamespacedService(namespace||'default');
+    const res = await client.listNamespacedService(namespace || "default");
     return res.body.items;
-  }
-  catch (err){
+  } catch (err) {
     throw parseErr(err);
   }
 }
 
-async function getService(action, settings){
-  const {name, namespace} = action.params;
-  if (!name){
+async function getService(action, settings) {
+  const { name, namespace } = action.params;
+  if (!name) {
     throw "Didn't provide service name";
   }
   const kc = getConfig(action.params, settings);
@@ -121,8 +121,7 @@ async function getService(action, settings){
   try {
     const res = await client.readNamespacedService(name, namespace || "default");
     return res.body;
-  }
-  catch (err){
+  } catch (err) {
     throw parseErr(err);
   }
 }
@@ -133,6 +132,5 @@ module.exports = {
   getService,
   getAllServices,
   // CLI methods
-  ...require("./app.cli")
+  ...cliApp,
 };
-
