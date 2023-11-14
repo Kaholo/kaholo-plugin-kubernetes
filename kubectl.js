0b0622bc81f645349b499f98655dee1ae8e7b94c
Kenny Marsh <kenny.m@kaholo.io>
2023-11-14 13:06:37 +0100

    tested working with account or using param

--- kubectl.js
+++ kubectl.js
@@ -23,17 +23,18 @@ async function runCommand(params) {
   const {
     kubeCertificate,
     eksToken,
+    kubeToken,
     kubeApiServer,
     command: usersCommand,
     namespace,
   } = params;
 
-  const kubeToken = eksToken;
+  const useToken = eksToken || kubeToken;
   const base64Certificate = convertCertificateToBase64(kubeCertificate);
 
   const shellEnvironmentalVariables = {};
   shellEnvironmentalVariables[environmentalVariablesNames.kubeCertificate] = base64Certificate;
-  shellEnvironmentalVariables[environmentalVariablesNames.kubeToken] = kubeToken;
+  shellEnvironmentalVariables[environmentalVariablesNames.kubeToken] = useToken;
   shellEnvironmentalVariables[environmentalVariablesNames.kubeApiServer] = kubeApiServer;
   if (namespace) {
     shellEnvironmentalVariables[environmentalVariablesNames.namespace] = namespace;
